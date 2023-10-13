0443f0f6674392d97c1d69b807f0e76fde6cacd0
xaviervergara <xaviervergara00@gmail.com>
2023-10-13 01:28:48 -0300

    arreglo problema fetching json local

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -2,6 +2,7 @@ import CardProduct from '../../components/CardProduct/CardProduct';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+// import asd from '../../../public/productos.json';
 
 const DetailPage = () => {
   let { id } = useParams();
@@ -10,31 +11,25 @@ const DetailPage = () => {
   // console.log(producto);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(`/public/productos.json/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => setProducto(data));
-    }, 500);
+    fetch(`/public/productos.json`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application.json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setTimeout(() => {
+          setProducto(data.filter((prod) => prod.id == id)[0]);
+        }, 500)
+      );
   }, [id]);
 
   return (
     <Grid container>
-      <h1>hello</h1>
+      <CardProduct producto={producto} />
     </Grid>
   );
 };
 
 export default DetailPage;
-
-// {productos.map((producto) => {
-//     return (
-//       <div key={producto.id}>
-//         <CardProduct producto={producto} />
-//       </div>
-//     );
-//   })}
