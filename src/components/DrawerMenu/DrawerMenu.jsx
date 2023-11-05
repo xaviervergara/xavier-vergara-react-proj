65a42a50b6a5a0ee4ac600d75bbc38186e79b464
xaviervergara <xaviervergara00@gmail.com>
2023-11-05 19:16:33 -0300

    implementacion de formulario con libreria react-hook-form en pagina de contacto

--- src/components/DrawerMenu/DrawerMenu.jsx
+++ src/components/DrawerMenu/DrawerMenu.jsx
@@ -1,17 +1,13 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import IconoCubo from '../IconoCubo/IconoCubo';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import CategoryIcon from "@mui/icons-material/Category";
+import IconoCubo from "../IconoCubo/IconoCubo";
+import { Link } from "react-router-dom";
 
-import './DrawerMenu.css';
+import "./DrawerMenu.css";
 
 const DrawerMenu = () => {
   const [state, setState] = React.useState({
@@ -20,8 +16,8 @@ const DrawerMenu = () => {
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
     ) {
       return;
     }
@@ -32,9 +28,9 @@ const DrawerMenu = () => {
   const list = (anchor) => (
     <Box
       sx={{
-        width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
-        backgroundColor: 'rgb(255, 255, 255)',
-        height: '100%',
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+        backgroundColor: "rgb(255, 255, 255)",
+        height: "100%",
       }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
@@ -43,16 +39,16 @@ const DrawerMenu = () => {
       <List>
         <div
           style={{
-            height: '2em',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '5px',
-            color: 'rgb(228, 184, 65)',
-            fontSize: '2em',
-            margin: '1.5em',
-            fontFamily: 'monospace',
-            backgroundColor: 'rgb(97, 172, 241)',
+            height: "2em",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "5px",
+            color: "white",
+            fontSize: "2em",
+            margin: "1.5em",
+            fontFamily: "monospace",
+            backgroundColor: "rgb(97, 172, 241)",
           }}
         >
           Categorias
@@ -87,25 +83,25 @@ const DrawerMenu = () => {
 
   return (
     <div>
-      {['left'].map((anchor) => (
+      {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
           <Button
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              color: 'rgb(230, 96, 96)',
-              backgroundColor: 'rgb(255, 255, 255)',
-              borderRadius: '5px',
-              height: '2em',
-              fontSize: '1.7em',
-              fontFamily: 'monospace',
-              fontWeight: '500',
-              width: '8%',
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "rgb(230, 96, 96)",
+              backgroundColor: "rgb(255, 255, 255)",
+              borderRadius: "5px",
+              height: "2em",
+              fontSize: "1.7em",
+              fontFamily: "monospace",
+              fontWeight: "500",
+              // width: "8%",
             }}
             onClick={toggleDrawer(anchor, true)}
           >
-            <MenuIcon />
+            <CategoryIcon />
           </Button>
           <Drawer
             anchor={anchor}
@@ -121,14 +117,3 @@ const DrawerMenu = () => {
 };
 
 export default DrawerMenu;
-
-// {['2x2', '3x3', '4x4', '5x5'].map((text, index) => (
-//   <ListItem key={text} disablePadding>
-//     <ListItemButton>
-//       <ListItemIcon>
-//         {index % 2 === 0 ? <IconoCubo /> : <IconoCubo />}
-//       </ListItemIcon>
-//       <ListItemText primary={text} />
-//     </ListItemButton>
-//   </ListItem>
-// ))}
--- src/pages/ContactoPage/ContactoPage.jsx
+++ src/pages/ContactoPage/ContactoPage.jsx
@@ -1,16 +1,48 @@
+import "./ContactoPage.css";
+import { useForm } from "react-hook-form";
+
 const ContactoPage = () => {
+  const { register, handleSubmit } = useForm();
+
+  const enviar = (data) => {
+    console.log(data);
+  };
+
   return (
-    <h1
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        fontFamily: 'monospace',
-        fontSize: '3em',
-        color: 'yellow',
-      }}
-    >
-      Bienvenid@ a la página de contacto!
-    </h1>
+    <div className="mainFormulario">
+      <h1 className="main-title">Contacto</h1>
+      <form
+        onSubmit={handleSubmit(enviar)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Ingresá tu nombre"
+          {...register("nombre")}
+        />
+        <input
+          className="form-input"
+          type="email"
+          placeholder="Ingresá tu e-mail"
+          {...register("email")}
+        />
+
+        <input
+          className="form-input"
+          type="phone"
+          placeholder="Ingresá tu telefono"
+          {...register("telefono")}
+        />
+        <button className="boton-formulario" type="submit">
+          Enviar
+        </button>
+      </form>
+    </div>
   );
 };
 
